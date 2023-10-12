521bc9ab3c1b5ccfb41a82218ecdf6dc3f6c109a
parK-dev <kirk.paradis@gmail.com>
2023-10-12 17:53:41 +0900

    Initial Commit

---
+++ astro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  astro
+//
+//  Created by Kirk Paradis on 2023/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ astro/astroApp.swift
@@ -0,0 +1,17 @@
+//
+//  astroApp.swift
+//  astro
+//
+//  Created by Kirk Paradis on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct astroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
